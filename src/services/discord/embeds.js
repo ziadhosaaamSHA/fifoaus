13087b9ae625bf1be9a7201a461a492bad833dc0
Ziad Hossam <ziadhh2003@gmail.com>
2026-06-11 10:21:50 +0300

    Show publish time in news embeds

--- src/services/discord/embeds.js
+++ src/services/discord/embeds.js
@@ -102,6 +102,9 @@ function formatNewsDate(value) {
     day: "2-digit",
     month: "short",
     year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZoneName: "short",
     timeZone: "Australia/Perth"
   }).format(date);
 }
